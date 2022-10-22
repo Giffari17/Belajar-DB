fa6da4175f01bbc2048b85c7ecd75d30e208120f
Giffari17 <gyusrulkhoi@gmail.com>
2022-10-22 09:52:46 +0700

    add home user and detail user page

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,19 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeUser from '../views/HomeUser.vue'
+import DetailUser from '../views/DetailUser.vue'
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: '/users',
+    name: 'user',
+    component: HomeUser
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/users/:id',
+    name: 'user-detail',
+    component: DetailUser
   }
 ]
 
---
+++ client/src/views/HomeUser.vue
@@ -0,0 +1,91 @@
+<template>
+    <div class="container">
+        <h1>---Users list---</h1>
+        <div style="margin-bottom: 20px;" v-for="user, i in users" :key="i">
+            <div class="profile">
+                <div>
+                    <div class="profile-data-row">
+                        <p style="margin-right: 10px;">Name:</p>
+                        <p>{{ user.name }}</p>
+                    </div>
+                    <div class="profile-data-row">
+                        <p style="margin-right: 10px;">Address:</p>
+                        <p>{{ user.address }}</p>
+                    </div>
+                </div>
+                <div class="button-profile">
+                    <button class="button-element" style="margin-bottom: 10px;"
+                    @click="detailUser(user.id)">Detail</button>
+                    <button class="button-element" style="background-color: red;"
+                    @click="deleteUser(user.id)">Delete</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      users: [
+        {
+          id: 1,
+          name: 'udin',
+          address: 'sleman'
+        },
+        {
+          id: 2,
+          name: 'udin1',
+          address: 'kaliurang'
+        },
+        {
+          id: 3,
+          name: 'udin2',
+          address: 'monjali'
+        }
+      ]
+    }
+  },
+  methods: {
+    detailUser (id) {
+      this.$router.push(`/users/${id}`)
+    },
+    deleteUser (id) {
+      console.log(`Delete Feature for user id = ${id}`)
+    }
+  }
+}
+</script>
+<style scoped>
+.container {
+    margin: 100px;
+    border: 1px solid black;
+    height: 600px;
+    width: 600px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+.profile {
+    display: flex;
+    align-items: center;
+}
+.button-profile {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+.button-element {
+    padding: 10px 30px;
+    background-color: #80BDE3;
+    color: white;
+}
+.profile-data-row {
+    width: 300px;
+    display: flex;
+    font-size: 20px;
+}
+</style>
