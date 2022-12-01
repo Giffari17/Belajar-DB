078be7c37b76415318598f7da06fb0f34a1b50cb
b.d.ananta <b.d.ananta@gmail.com>
2022-12-02 04:10:37 +0700

    WEB v1

---
+++ .next/Backend/koneksi.php
@@ -0,0 +1,9 @@
+<?php
+  
+$dbHost = 'localhost';
+$dbName = 'radionuklidadb'; //ubah dengan nama database Anda
+$dbUsername = 'root';
+$dbPassword = '';
+  
+$mysqli = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName);
+?>
